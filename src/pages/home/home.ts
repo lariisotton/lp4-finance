a5950774726d8ff9cc656883f9f1ec098d57976b
Lariisotton <lariisotton@gmail.com>
2018-11-12 21:30:22 -0200

    continuacao lp4 dia 12/11

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,68 @@ import{ LancamentoAddPage } from '../lancamento-add/lancamento-add';
 })
 export class HomePage {
 
+  public referencia_mes: number = 0;
+  public referencia_ano: number = 0;
+  public mes: string = '';
+
   constructor(public navCtrl: NavController) {
+    let date = new Date();
+    this.referencia_mes = date.getMonth();
+    this.referencia_ano = date.getFullYear();
+    }
+
+
+  ionViewDidEnter() {
+  // this.getListCred();
+  // this.getListDeb();
+  this.getMes(this.referencia_mes);
+}
+
+
+getMes(mes) {
+    switch (mes) {
+      case 0:
+        this.mes = "Janeiro";
+        break;
+      case 1:
+        this.mes = "Fevereiro";
+        break;
+      case 2:
+        this.mes = "Março";
+        break;
+      case 3:
+        this.mes = "Abril";
+        break;
+      case 4:
+        this.mes = "Maio";
+        break;
+      case 5:
+        this.mes = "Junho";
+        break;
+      case 6:
+        this.mes = "Julho";
+        break;
+      case 7:
+        this.mes = "Agosto";
+        break;
+      case 8:
+        this.mes = "Setembro";
+        break;
+      case 9:
+        this.mes = "Outubro";
+        break;
+      case 10:
+        this.mes = "Novembro";
+        break;
+      case 11:
+        this.mes = "Dezembro";
+        break;
+
+      default:
+        this.mes = "";
+        break;
+    }
+  }
 
   }
 
